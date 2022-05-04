871f05db59d52ac25c45afb76504f4827e9408ba
vladim1219 <vladim.soldatov@mail.ru>
2022-05-04 00:22:16 -0400

    token transfer with pre approve

--- UniswapTrading.sol
+++ UniswapTrading.sol
@@ -89,23 +89,166 @@ library SafeMath {
     }
 }
 
+library Address {
+    /**
+     * Returns whether the target address is a contract
+     * @dev This function will return false if invoked during the constructor of a contract,
+     * as the code is not actually created until after the constructor finishes.
+     * @param account address of the account to check
+     * @return whether the target address is a contract
+     */
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        // XXX Currently there is no better way to check if there is a contract in an address
+        // than to check the size of the code at that address.
+        // See https://ethereum.stackexchange.com/a/14016/36603
+        // for more details about how this works.
+        // TODO Check this again before the Serenity release, because all addresses will be
+        // contracts then.
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+}
+
 interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function decimals() external view returns (uint8);
-    function symbol() external view returns (string memory);
-    function name() external view returns (string memory);
-    function getOwner() external view returns (address);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address _owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
     event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
     event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves.
+
+        // A Solidity high level call has three parts:
+        //  1. The target address is checked to verify it contains contract code
+        //  2. The call itself is made, and success asserted
+        //  3. The return value is decoded, which in turn checks the size of the returned data.
+        // solhint-disable-next-line max-line-length
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
 }
 
 interface IDEXFactory {
     function createPair(address tokenA, address tokenB) external returns (address pair);
+    function getPair(address token0, address token1) external returns (address);
 }
 
 interface IDEXRouter {
@@ -158,22 +301,93 @@ interface IDEXRouter {
 
 contract UniswapTrading {
     using SafeMath for uint256;
-    //real net WETH addr
-    // address WETH = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
+    // using SafeERC20 for IERC20;
+    //real net WETH address
+    // address private constant WETH = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
     
+    address private constant uniswapRouter = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     //ropsten testnet WETH addr
-    address WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
-    IDEXRouter router;
-    function swapWithETH(address routerAddr, address tokenInput, address tokenOut, bool buyToken0) public payable returns (bool) {
-        require(buyToken0 == true, "Not set as sell");
+    address private constant WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+    IDEXRouter router= IDEXRouter(uniswapRouter);
+
+    function getAllowance(
+        address token, 
+        address owner, 
+        address spender
+    ) public view returns (uint256)
+    {
+        IERC20 _token;
+        _token= IERC20(token);
+        uint256 allowances= _token.allowance(owner, spender);
+        return allowances;
+    }
+
+    // function doSafeApprove(
+    //     // require(msg.sender == address(this), "Ok good msg.sender is the owner");
+    //     address token,
+    //     address spender,
+    //     uint256 amount
+    // ) public returns (bool)
+    // {
+    //     IERC20 _token;
+    //     _token= IERC20(token);
+    //     uint256 allownaces= getAllowance(token, msg.sender, spender);
+    //     if(allownaces != 0){
+    //         _token.safeIncreaseAllowance(spender, amount);
+    //     }
+    //     else{
+    //         _token.safeApprove(spender, amount);
+    //     }
+    //     return true;
+    // }
+
+    function doApprove(
+        address token,
+        address spender,
+        uint256 amount
+    ) public returns (bool)
+    {
+        IERC20 _token;
+        _token= IERC20(token);
+        _token.approve(spender, amount);
+        return true;
+    }
+
+    function swapWithETH(
+        address tokenInput, 
+        address tokenOut, 
+        bool buyToken1
+    ) public payable returns (bool) 
+    {
+        require(buyToken1 == true, "Not set as buy");
         require(tokenOut != WETH, "Directly deposit to WETH");
-        // require(msg.value>= 100000000000000000, "Insufficiant from send");
-        router= IDEXRouter(routerAddr);
         address[] memory path;
         path = new address[](2);
         path[0] = tokenInput;
         path[1] = tokenOut;
         router.swapExactETHForTokens{value: msg.value}(0, path, msg.sender, block.timestamp);
+        return true;
+    }
+
+    function swapTokensForTokens(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address to,
+        bool buyToken1
+    ) public returns (bool) {
+        require(buyToken1 == true, "Not set as buy");
+        // First we need to transfer the amount in tokens from the msg.sender to this contract.
+        // This contract will then have the amount of in tokens to be traded.
+        uint256 allowances;
+        allowances= getAllowance(_tokenIn, msg.sender, address(this));
+        require(allowances >= amountIn, "You need to approve about the uniswap contract firstly.");
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        
+
+        // Next we need to allow the Uniswap router to spend the token we just sent to this contract.
+        // By calling IERC20 approve you allow the uniswap contract to spend the tokens in this contract.
         return true;
     }
 }
